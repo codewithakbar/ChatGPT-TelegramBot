c7963cf0c4e9d671a77baf55e4619b71417b2219
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-03-23 17:44:47 +0500

    remove

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import openai
 
 # Set up the bot and OpenAI API credentials
 bot_token = '5680003803:AAHZ5BCGQMIYmzgn1mk77jRH5UW1GJgASfQ'
-api_key = "'91=jjp#g0-mbdfntgww91&s9b^a!kj44lj14f5h!+uoftshбu"
+api_key = "91=jjp#g0-mbdfntgww91&s9b^a!kj44lj 14f5h!+uoftshбu"
 
 logging.basicConfig(level=logging.INFO)
 
